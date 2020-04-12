20abdc1c3be38583dfb3d9054df57c727f30f106
Eryk Rachwal <erykrachwal@yahoo.com>
2020-04-12 01:34:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::
+  
   get '/' do 
 
     erb :user_input
   end
+ 
   post '/piglatinize' do 
     instance = PigLatinizer.new
     @piglatinized_stuff = instance.piglatinize(params[:user_phrase])
